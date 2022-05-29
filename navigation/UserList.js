102cc3dd5d2e462017009bb22be3401e3c0c3850
Gülçin Özpamuk <gulcinozpamuk@hotmail.com>
2022-05-29 23:12:33 +0300

    User List Sayfasındaki Kullanıcılar Listelendi.

--- navigation/UserList.js
+++ navigation/UserList.js
@@ -1,10 +1,34 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { API_URL } from '../env/config';
+import { FlatList } from 'react-native-gesture-handler';
 
 const UserList = () => {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch(API_URL + "users")
+        .then(res => res.json())
+        .then((data) => {
+            setUsers(data);
+        })
+    }, [])
+    
+    const renderUser = ({item}) => {
+        return <Text> 
+            name: {item.name}             
+        </Text>
+    }
+
   return (
     <View>
-      <Text>UserList</Text>
+        <FlatList
+        data={users}
+        renderItem={renderUser}
+        >
+
+        </FlatList>
     </View>
   )
 }
